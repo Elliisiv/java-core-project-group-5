d939640fee9391829816d8eb1d6ddddc43499f52
Yelyzaveta Buhaichuk <bug.liza@gmail.com>
2023-05-19 17:41:43 +0300

    Create User and Settings

---
+++ src/main/java/com/telegramBot/User/UserSettings.java
@@ -0,0 +1,14 @@
+package com.telegramBot.User;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class UserSettings {
+    private static final String SETTINGS_FILE_PATH = "./files/userSettings.json";
+    private Gson gson;
+
+}

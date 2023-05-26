ff6a068e76fc1ea813716edc1913a5ca818b3739
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-27 01:36:23 +0300

    Add files via upload

---
+++ src/main/java/com/telegramBot/bank/Privat/GetCurrencyPrivatbank.java
@@ -0,0 +1,112 @@
+package com.telegramBot.bank.Privat;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.telegramBot.bank.BankEnum;
+import lombok.Value;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.text.DecimalFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.math.BigDecimal;
+
+
+
+public class GetCurrencyPrivatbank {
+
+    public static String desimalCode;//Формат округлення
+    public  static BigDecimal buy;//Покупка валюти
+    public static BigDecimal sell;//Продаж валюти
+
+    public static void main(String[] args) throws IOException {
+
+        String result = GetExchangePrivatbank(BankEnum.USD, 2);
+
+    }
+
+//    public static double getCurrencySell(BankEnum currency) throws IOException {
+//
+//        double CurrencySell = Double.parseDouble(String.valueOf(GetExchangePrivatbank(String.valueOf(currency)).get_saleRate()));
+//
+//        System.out.println("CurrencySell: " + CurrencySell);
+//        return CurrencySell;
+//    }
+//
+//    public static double getCurrencyBuy (BankEnum currency) throws IOException {
+//        double CurrencyBuy = Double.parseDouble(String.valueOf(GetExchangePrivatbank(String.valueOf(currency)).get_purchase()));
+//
+//        System.out.println("CurrencyBuy: " + CurrencyBuy);
+//        return CurrencyBuy;
+//
+//    }
+
+    public static String GetExchangePrivatbank(BankEnum currency, int number) throws IOException {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
+        String URL_get_currency = "https://api.privatbank.ua/p24api/exchange_rates?json&date=" + dateFormat.format(new Date() );
+
+        Privatbank result = new Privatbank();
+
+        URL url = new URL(URL_get_currency);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Content-Type", "application/json");
+
+        int responseCode = connection.getResponseCode();
+        System.out.println("GET response code: " + responseCode);
+
+        if (responseCode == HttpURLConnection.HTTP_OK) {
+            BufferedReader in =
+                    new BufferedReader(
+                            new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+
+            Gson gsonR = new GsonBuilder().setPrettyPrinting().create();
+            JsonObject jsonObject = gsonR.fromJson(response.toString(), JsonObject.class);
+            JsonArray exchangeRateArray = jsonObject.getAsJsonArray("exchangeRate");
+
+            Privatbank[] currencyList = gsonR.fromJson(exchangeRateArray, Privatbank[].class);
+
+
+            for (Privatbank currencyNeeded : currencyList) {
+                if(currency.equals(currencyNeeded.get_currency())){
+                    result = currencyNeeded ;
+                }
+            }
+            int a = 5;
+
+        } else {
+            System.out.println("GetExchangePrivateBank request not worked");
+        }
+
+        buy = result.get_saleRate();
+        sell = result.get_purchase();
+
+        switch (number) {
+            case 2 -> desimalCode="#.##";
+            case 3 -> desimalCode="#.###";
+            case 4 -> desimalCode="#.####";
+            default -> desimalCode="#.#";
+        }
+        DecimalFormat decimalFormat = new DecimalFormat(desimalCode);
+        String resultForSell = decimalFormat.format(sell);
+        String resultForBuy = decimalFormat.format(buy);
+
+        String resultPrivate = "Курс в Приват банк: "+currency+"/UAH\nПокупка: "+resultForSell+"\nПродажа: "+resultForBuy;
+
+        return resultPrivate;
+    }
+}
--- src/main/java/com/telegramBot/bank/Privat/Privatbank.java
+++ src/main/java/com/telegramBot/bank/Privat/Privatbank.java
@@ -1,30 +1,22 @@
 package com.telegramBot.bank.Privat;
+import com.telegramBot.bank.BankEnum;
+import com.telegramBot.bank.BankEnumFormatter;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
-import java.util.Currency;
-import java.util.Date;
+import java.math.BigDecimal;
 
 public class Privatbank {
 
     private String baseCurrency;
-    private String currency;
-    private String purchaseRate;
-    private String saleRate;
+    private BankEnum currency;
+    private BigDecimal purchaseRate;
+    private BigDecimal saleRate;
 
 
-    public void Privatbank(String baseCurrency, String currency, String purchaseRate, String saleRate){
+    public void Privatbank(String baseCurrency, String currency, BigDecimal purchaseRate, BigDecimal saleRate){
         this.baseCurrency = baseCurrency;
-        this.currency = currency;
+        BankEnumFormatter bankFormatter = new BankEnumFormatter();
+        BankEnum currencyN = bankFormatter.getBankEnumValue(currency);
+        this.currency = currencyN;
         this.purchaseRate = purchaseRate;
         this.saleRate = saleRate;
 
@@ -33,84 +25,14 @@ public class Privatbank {
     public String get_base_currency() {
         return baseCurrency;
     }
-    public String get_currency() {
+    public BankEnum get_currency() {
         return currency;
     }
-    public String get_purchase() {
+    public BigDecimal get_purchase() {
         return purchaseRate;
     }
 
-}
-
-
-class GetCurrencyPrivatBank {
-
-    public static void main(String[] args) throws IOException {
-
-        Privatbank result = GetExchangePrivateBank("USD");
-
-    }
-    
-        public static double getCurrencySell(BankEnum currency) throws IOException {
-
-        double CurrencySell = Double.parseDouble(String.valueOf(GetExchangePrivateBank(currency).get_saleRate()));        
-        
-        System.out.println("CurrencySell: " + CurrencySell);
-        return CurrencySell;
-    }
-
-    public static double getCurrencyBuy (BankEnum currency) throws IOException {
-        double CurrencyBuy = Double.parseDouble(String.valueOf(GetExchangePrivateBank(currency).get_purchase()));
-
-        System.out.println("CurrencyBuy: " + CurrencyBuy);
-        return CurrencyBuy;
-        
-    }
-
-    public static Privatbank GetExchangePrivateBank(String currency) throws IOException {
-        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy");
-        String URL_get_currency = "https://api.privatbank.ua/p24api/exchange_rates?json&date=" + dateFormat.format(new Date() );
-//       String URL_get_currency = "https://api.privatbank.ua/p24api/exchange_rates?json&date=21.05.2023" ;
-
-        Privatbank result = new Privatbank();
-
-        URL url = new URL(URL_get_currency);
-        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-        connection.setRequestMethod("GET");
-        connection.setRequestProperty("Content-Type", "application/json");
-
-        int responseCode = connection.getResponseCode();
-        System.out.println("GET response code: " + responseCode);
-
-        if (responseCode == HttpURLConnection.HTTP_OK) {
-            BufferedReader in =
-                    new BufferedReader(
-                            new InputStreamReader(connection.getInputStream()));
-            String inputLine;
-
-            StringBuffer response = new StringBuffer();
-            while ((inputLine = in.readLine()) != null) {
-                response.append(inputLine);
-            }
-            in.close();
-
-            Gson gsonR = new GsonBuilder().setPrettyPrinting().create();
-            JsonObject jsonObject = gsonR.fromJson(response.toString(), JsonObject.class);
-            JsonArray exchangeRateArray = jsonObject.getAsJsonArray("exchangeRate");
-
-            Privatbank[] currencyList = gsonR.fromJson(exchangeRateArray, Privatbank[].class);
-
-
-            for (Privatbank currencyNeeded : currencyList) {
-                if(currency.equals(currencyNeeded.get_currency())){
-                    result = currencyNeeded ;
-                }
-            }
-            int a = 5;
-
-        } else {
-            System.out.println("GetExchangePrivateBank request not worked");
-        }
-        return result;
+    public BigDecimal get_saleRate() {
+        return saleRate;
     }
 }

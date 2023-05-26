6bedc80988dfc526be1f8e72ad723647a28485a1
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-27 01:44:19 +0300

    Update BankNBU.java
    Змінила тип повернення на рядок(щоб працював загальний клас отримати інфо) і додала кількість знаків після коми

--- src/main/java/com/telegramBot/bank/NBU/BankNBU.java
+++ src/main/java/com/telegramBot/bank/NBU/BankNBU.java
@@ -1,4 +1,54 @@
 package com.telegramBot.bank.NBU;
 
-public class BankNBU {
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.telegramBot.bank.BankEnum;
+import org.jsoup.Jsoup;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.text.DecimalFormat;
+import java.util.List;
+
+
+public class NBU {
+    public static String desimalCode;//Формат округлення
+    private static String url = "https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json";
+    public static String getCurrencyRate(BankEnum currency, int number) {
+
+        String json;
+        {
+            try {
+                json = Jsoup.connect(url)
+                        .ignoreContentType(true)
+                        .get()
+                        .body()
+                        .text();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        Type type = TypeToken.getParameterized(List.class, NBUdto.class).getType();
+        List<NBUdto> items = new Gson().fromJson(json, type);
+
+         double rez = items.stream()
+                .filter(item -> item.getCc() == currency)
+                .map(item -> item.getRate())
+                .findFirst()
+                .orElseThrow();
+
+            switch (number) {
+                case 2 -> desimalCode="#.##";
+                case 3 -> desimalCode="#.###";
+                case 4 -> desimalCode="#.####";
+                default -> desimalCode="#.#";
+            }
+            DecimalFormat decimalFormat = new DecimalFormat(desimalCode);
+            String result = decimalFormat.format(rez);
+
+
+            String resultNBU = "Курс в НБУ: "+currency+"/UAH\n"+ result ;
+
+         return resultNBU ;
+    }
 }

d04f4a57fc302bdefb0a0c5e7eef159b2b8fba26
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-27 17:47:12 +0300

    Update User.java

--- src/main/java/com/telegramBot/User/User.java
+++ src/main/java/com/telegramBot/User/User.java
@@ -4,7 +4,7 @@ import lombok.Data;
 @Data
 public class User {
     private long chatId;
-    private String bank;
+    private String [] banks;
     private String[] currencies;
     private int rounding;
     private String time;
@@ -16,9 +16,9 @@ public class User {
     }
     ///////////////////////////////////////////////
 
-    public User(long chatId, String bank, String[] currencies, int rounding, String time) {
+    public User(long chatId, String[] banks, String[] currencies, int rounding, String time) {
         this.chatId = chatId;
-        this.bank = bank;
+        this.banks = banks;
         this.currencies = currencies;
         this.rounding = rounding;
         this.time = time;

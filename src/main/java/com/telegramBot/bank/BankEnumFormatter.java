501789ae7e8b456c70e006d50147ad8cefdf7d1d
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-27 19:27:46 +0300

    Update BankEnumFormatter.java

--- src/main/java/com/telegramBot/bank/BankEnumFormatter.java
+++ src/main/java/com/telegramBot/bank/BankEnumFormatter.java
@@ -3,16 +3,12 @@ package com.telegramBot.bank;
 public class BankEnumFormatter {
     public BankEnum getBankEnumValue(String enam) {
         switch (enam) {
-            case "EUR":
-                return BankEnum.EUR;
-            case "UAH":
-                return BankEnum.UAH;
-            case "USD":
-                return BankEnum.USD;
-            case "GBP":
-                return BankEnum.GBP;
-            case "PLN":
-                return BankEnum.PLN;
+            case "Privat":
+                return BankEnum.Privat;
+            case "Mono":
+                return BankEnum.Mono;
+            case "NBU":
+                return BankEnum.NBU;
         }
 
         throw new IllegalArgumentException("We don't know enam for " + enam);

3644210a97a6961e716e1cfe223fc0125b9e89a3
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-27 17:23:20 +0300

    Add files via upload

---
+++ src/main/java/com/telegramBot/bank/NBU/NBUdto.java
@@ -0,0 +1,24 @@
+package com.telegramBot.bank.NBU;
+
+import com.telegramBot.bank.BankEnum;
+import com.telegramBot.bank.CurrencyEnum;
+import lombok.Data;
+
+@Data
+public class NBUdto {
+    private String r030;
+    private String txt;
+    private double rate;
+    private CurrencyEnum cc;
+    private String exchangedate;
+    
+    ///////////////////////////////////////////////
+    // Ira.Y.
+    public CurrencyEnum getCc() {
+        return cc;
+    }
+    public double getRate() {
+        return rate;
+    }
+    ///////////////////////////////////////////////
+}

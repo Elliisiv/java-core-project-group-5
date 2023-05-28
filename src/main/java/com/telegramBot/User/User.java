871b0ff802b8157dbf9d33efd24a72d8b94a46df
Yelyzaveta Buhaichuk <bug.liza@gmail.com>
2023-05-28 18:00:17 +0300

    Update User.java

--- src/main/java/com/telegramBot/User/User.java
+++ src/main/java/com/telegramBot/User/User.java
@@ -6,13 +6,13 @@ import java.util.Arrays;
 @Data
 public class User {
     private long chatId;
-    private String bank;
+    private String[] bank;
     private String[] currencies;
     //замінити на ліст?
     private int rounding;
     private String time;
 
-    public User(long chatId, String bank, String[] currencies, int rounding, String time) {
+    public User(long chatId, String[] bank, String[] currencies, int rounding, String time) {
         this.chatId = chatId;
         this.bank = bank;
         this.currencies = currencies;
@@ -24,7 +24,7 @@ public class User {
     public String toString() {
         return "User{" +
                 "chatId=" + chatId +
-                ", bank='" + bank + '\'' +
+                ", bank=" + Arrays.toString(bank) +
                 ", currencies=" + Arrays.toString(currencies) +
                 ", rounding=" + rounding +
                 ", time='" + time + '\'' +

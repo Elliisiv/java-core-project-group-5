c724be1596a0e72ba3754b9544e1af1c896c6cc8
NinaSemnul <124104416+NinaSemnul@users.noreply.github.com>
2023-05-29 17:49:59 +0300

    Update DecimalPlaces.java

--- src/main/java/com/telegramBot/telegram/buttons/DecimalPlaces.java
+++ src/main/java/com/telegramBot/telegram/buttons/DecimalPlaces.java
@@ -1,5 +1,7 @@
 package com.telegramBot.telegram.buttons;
 
+import com.telegramBot.User.User;
+import com.telegramBot.User.UserSettings;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardButton;
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow;
@@ -8,20 +10,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class DecimalPlaces {
-    public static ReplyKeyboardMarkup getDecimalPlaces() {
+    public static ReplyKeyboardMarkup getDecimalPlaces(long chatId) {
         ReplyKeyboardMarkup replyMarkup = new ReplyKeyboardMarkup();
         replyMarkup.setResizeKeyboard(true);
         replyMarkup.setSelective(true);
-        replyMarkup.setKeyboard(getDecimalPlacesButtons());
+        replyMarkup.setKeyboard(getDecimalPlacesButtons(chatId));
 
         return replyMarkup;
     }
 
-    private static List<KeyboardRow> getDecimalPlacesButtons() {
+    private static List<KeyboardRow> getDecimalPlacesButtons(long chatId) {
+        UserSettings userSettings = new UserSettings();
+        User user = userSettings.getUserSettingsByChatId(chatId);
+
         KeyboardRow row1 = new KeyboardRow();
-        row1.add(new KeyboardButton("2"));
-        row1.add(new KeyboardButton("3"));
-        row1.add(new KeyboardButton("4"));
+        if(user.getRounding() == 2) {
+            row1.add(new KeyboardButton("2 ✅"));
+            row1.add(new KeyboardButton("3"));
+            row1.add(new KeyboardButton("4"));
+        }
+        if(user.getRounding() == 3) {
+            row1.add(new KeyboardButton("2"));
+            row1.add(new KeyboardButton("3 ✅"));
+            row1.add(new KeyboardButton("4"));
+        }
+        if(user.getRounding() == 4) {
+            row1.add(new KeyboardButton("2"));
+            row1.add(new KeyboardButton("3"));
+            row1.add(new KeyboardButton("4 ✅"));
+        }
 
         KeyboardRow row2 = new KeyboardRow();
         row2.add(new KeyboardButton("Назад"));

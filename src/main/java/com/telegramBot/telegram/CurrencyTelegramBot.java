f5f75e1a01b3491b57090684f7398412a6e11de7
olye <olexiy.yevdokimov.2000@gmail.com>
2023-05-30 23:57:43 +0300

    Checkboxes added everywhere

--- src/main/java/com/telegramBot/telegram/CurrencyTelegramBot.java
+++ src/main/java/com/telegramBot/telegram/CurrencyTelegramBot.java
@@ -82,9 +82,11 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
                     }
                     if (isRoundingButton(messageText)) {
                         buttonHandler.handleRoundingButton(messageText, chatId);
+                        handleDecimalPlacesSetting(chatId);
                     }
                     if (isTimeButton(messageText)) {
                         buttonHandler.handleTimeButton(messageText, chatId);
+                        handleNotificationTimeSetting(chatId);
                     }
                 }
             }
@@ -192,7 +194,7 @@ public class CurrencyTelegramBot extends TelegramLongPollingBot {
 
     private void handleNotificationTimeSetting(long chatId) {
         SendMessage message = createMessage(chatId, "Оберіть час сповіщень");
-        message.setReplyMarkup(NotificationTimeSetting.getNotificationTime());
+        message.setReplyMarkup(NotificationTimeSetting.getNotificationTime(chatId));
         sendMessage(message);
     }
 
